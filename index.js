366870061af368c6c6210dbc50e59dadd38103a3
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-10-25 12:29:03 +0600

    courses added

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const courses = require('./Data/courses.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello mada faka')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
